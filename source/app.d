8a4db2cb5370efcff58668ec91ccb188e5795bee
Kxoe <kxoethecat@gmail.com>
2018-08-18 09:44:31 -0600

    Deleted existing code in favor of a new project structure

--- source/app.d
+++ source/app.d
@@ -1,9 +1,8 @@
 module app;
 
-import graphics;
+import d2d;
 
 void main() {
-	Display display = new Display(800, 600, SDL_WINDOW_SHOWN, 0, "Anthropos", Image.Icon);
-	display.screen = new MainScreen(display);
+	Display display = new Display(800, 600, SDL_WINDOW_SHOWN, 0, "Anthropos", "res/icon.png");
 	display.run();
 }
--- source/graphics/package.d
+++
@@ -1,6 +0,0 @@
-module graphics;
-
-public import d2d;
-public import graphics.components.Map;
-public import graphics.Constants;
-public import graphics.MainScreen;
